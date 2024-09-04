7b9ad06101257741c3f36314f668d26519e290b5
xsamueljr <samueljiro.alfa@gmail.com>
2024-09-04 19:34:16 +0200

    feat: Tipos de productos más concretos

--- src/types/product.ts
+++ src/types/product.ts
@@ -2,3 +2,5 @@ import z from 'zod';
 import { productSchema } from '../schemas/product';
 
 export type Product = z.infer<typeof productSchema>;
+export type ProductWithoutId = Omit<Product, 'id'>;
+export type ProductUpdate = Omit<ProductWithoutId, 'disabled'>;
