e47d964316ae2acdf36a193af5983493817d519f
John <johnelvis963@gmail.com>
2024-09-07 15:42:23 -0500

    feat: implementado mensages para el manejo de errores

--- src/products/product.schema.ts
+++ src/products/product.schema.ts
@@ -1,18 +1,38 @@
 import z from 'zod';
 
-// número entero o decimal con 2 dígitos en la parte decimal
-// No he encontrado una forma mejor de implementarlo con Zod
-const priceVal = z.number().min(0).multipleOf(0.01);
+// Validar número con hasta dos decimales y positivo
+const priceVal = z.number({
+    required_error: '[price] es requerido',
+    invalid_type_error: '[price] debe ser un número decimal con hasta dos decimales',
+}).min(0).refine(val => Math.floor(val * 100) === val * 100, {
+    message: "El precio debe tener hasta dos decimales."
+});
 
 export const productSchema = z.object({
-    id: z.number().int(),
-    name: z.string().min(1).max(255),
-    description: z.string().min(1).max(500),
+    name: z.string({
+        required_error: '[name] es requerido',
+        invalid_type_error: '[name] debe ser una cadena',
+    }).min(1, "El nombre debe tener al menos 1 carácter").max(255, "El nombre no puede exceder los 255 caracteres"),
+
+    description: z.string({
+        required_error: '[description] es requerido',
+        invalid_type_error: '[description] debe ser una cadena',
+    }).min(1, "La descripción debe tener al menos 1 carácter").max(500, "La descripción no puede exceder los 500 caracteres"),
+
     price: priceVal,
-    stock: z.number().int().min(0),
-    isActive: z.boolean().default(true),
-    createdAt: z.date(),
-    updatedAt: z.date(),
+
+    stock: z.number({
+        required_error: '[stock] es requerido',
+        invalid_type_error: '[stock] debe ser un número entero',
+    }).int("El stock debe ser un número entero").min(0, "El stock no puede ser negativo"),
+
+    isActive: z.boolean({
+        invalid_type_error: '[isActive] debe ser booleano',
+    }).default(true).optional(),
+
+    imageIdsDelete: z.array(z.number({
+        invalid_type_error: '[imageIdsDelete] debe ser un array de números enteros',
+    })).optional(),
 });
 
 export const publicProductSchema = productSchema.pick({
@@ -23,4 +43,4 @@ export const publicProductSchema = productSchema.pick({
     isActive: true,
 });
 
-export const updateProductSchema = publicProductSchema.partial();
+export const updateProductSchema = productSchema.partial();
