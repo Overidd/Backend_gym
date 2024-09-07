7454a1f4c929a3e4e55afa1a32af2eef2e16024e
John <johnelvis963@gmail.com>
2024-09-07 15:40:45 -0500

    test

--- src/products/product.types.ts
+++
@@ -1,6 +0,0 @@
-import z from 'zod';
-import { productSchema, publicProductSchema, updateProductSchema } from './product.schema';
-
-export type Product = z.infer<typeof productSchema>;
-export type PublicProduct = z.infer<typeof publicProductSchema>;
-export type UpdateProduct = z.infer<typeof updateProductSchema>;
