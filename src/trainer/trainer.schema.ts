40596a21f7b770797a12b0bbad1ae058b83b31f2
Overidd <johnelvis963@gmail.com>
2024-10-07 17:00:32 -0500

    fix: router del trainer cambio de las especificaciones

--- src/trainer/trainer.schema.ts
+++ src/trainer/trainer.schema.ts
@@ -9,19 +9,20 @@ export const trainerSchema = z.object({
       required_error: '[last_name] es requerido',
       invalid_type_error: '[last_name] debe ser una cadena',
    }).max(255),
-
    email : z.string({
       required_error: '[email] es requerido',
       invalid_type_error: '[email] debe ser una dirección de correo electrónico',
    }).email({
       message: '[email] Debe ser una dirección de correo electrónico'
    }),
+   
    phone: z.string({
       required_error: '[phone] es requerido',
       invalid_type_error: '[phone] debe ser un número',
    }).max(10, {
       message: '[phone] debe tener como maximo 10 dígitos',
    }).optional(),
+   
    specialization: z.string({
       required_error: '[specialization] es requerido',
       invalid_type_error: '[specialization] debe ser una cadena',
