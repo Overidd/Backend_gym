9075ae9fdd9e57f09d04d4d66a4832281b304eff
Overidd <johnelvis963@gmail.com>
2024-10-18 11:56:50 -0500

    refactor:

--- src/presentation/trainer/schema.ts
+++ src/presentation/trainer/schema.ts
@@ -2,38 +2,38 @@ import { z } from 'zod';
 
 export const trainerSchema = z.object({
    first_name: z.string({
-      required_error: '[first_name] es requerido',
-      invalid_type_error: '[first_name] debe ser una cadena',
+      required_error: 'El Nombre es requerido',
+      invalid_type_error: 'El Nombre debe ser una cadena',
    }).max(255),
    last_name: z.string({
-      required_error: '[last_name] es requerido',
-      invalid_type_error: '[last_name] debe ser una cadena',
+      required_error: 'EL apellido es requerido',
+      invalid_type_error: 'EL apellido debe ser una cadena',
    }).max(255),
 
    email : z.string({
-      required_error: '[email] es requerido',
-      invalid_type_error: '[email] debe ser una dirección de correo electrónico',
+      required_error: 'Email es requerido',
+      invalid_type_error: 'Email debe ser una dirección de correo electrónico',
    }).email({
-      message: '[email] Debe ser una dirección de correo electrónico'
+      message: 'Email debe ser una dirección de correo electrónico'
    }),
    phone: z.string({
-      required_error: '[phone] es requerido',
-      invalid_type_error: '[phone] debe ser un número',
+      required_error: 'El teléfono es requerido',
+      invalid_type_error: 'El teléfono debe ser un número',
    }).min(6,{
-      message: '[phone] debe tener como minimo 6 dígitos',
+      message: 'El teléfono debe tener como minimo 6 dígitos',
    }).max(10, {
-      message: '[phone] debe tener como maximo 10 dígitos',
+      message: 'El teléfono debe tener como maximo 10 dígitos',
    }),
    specialization: z.string({
-      required_error: '[specialization] es requerido',
-      invalid_type_error: '[specialization] debe ser una cadena',
+      required_error: 'La especialización es requerido',
+      invalid_type_error: 'La especialización debe ser una cadena',
    }).min(5, {
-      message: '[specialization] debe tener como minimo 5 caracteres',
+      message: 'La especialización debe tener como minimo 5 caracteres',
    }).max(255, {
-      message: '[specialization] debe tener como maximo 255 caracteres',
+      message: 'La especialización debe tener como maximo 255 caracteres',
    }),
    description: z.string({
-      invalid_type_error: '[description] debe ser una cadena',
+      invalid_type_error: 'La description debe ser una cadena',
    }).max(500).optional(),
    isActive: z.boolean({
       invalid_type_error: '[isActive] debe ser booleano',
